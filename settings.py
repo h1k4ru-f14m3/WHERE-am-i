00d12d2c8feee3948b9bb4da417fce23216ecf02
MSI\CYBORG <hikaru.hino.flame@gmail.com>
2024-12-24 12:41:15 +0630

    Added Booleans for Game states

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Game States and Booleans
+
+onMainMenu = False
+onMainMap = False
+
+inTomDiner = False
+inMart = False
+inPolice1 = False
+inPolice2 = False
+
+inHouse1 = False
+inHouse2 = False
+inHouse3a = False
+inHouse3b = False
