cff8ce2a52c116296c3ea7dd1dcdf0bc2e6a6e85
MSI\CYBORG <hikaru.hino.flame@gmail.com>
2024-12-24 12:34:58 +0630

    New Resources

---
+++ resources/maps/Tilesets/Interiors-32x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Interiors-32x" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
+ <image source="Tilesets_Image/Interiors-32x.png" trans="ff00ff" width="512" height="2848"/>
+</tileset>
---
+++ resources/maps/Tilesets/Police-Tileset-V9.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Police-Tileset-V9" tilewidth="32" tileheight="32" tilecount="32" columns="4">
+ <image source="Tilesets_Image/tilemap-test-9.png" trans="ff00ff" width="128" height="256"/>
+</tileset>
